7fa6ce7c67da018d96297405f65d3d61e9f10aca
Kurvin Hendricks <kurvinhendricks@gmail.com>
2018-06-07 14:26:03 +0200

    adding index, built a little package json for the fans

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const port = 3000;
+
+//you can use the above to run your own server if you want, install express for some API action
+
+const RemoteInstance = require('directus-sdk-javascript/remote');
+
+const client = new RemoteInstance({
+  url: 'http://directus.properties.local/api/1.1',
+  accessToken: '7ElG7N6XM8ygNkljNqRnEEHC57HQ29C8'
+});
+
+console.log(client);
+
+client.getItems('properties_1')
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+
